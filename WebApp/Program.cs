4e4f615f2331e8b48579f463d728edbee86ee4f6
My <my.j97@protonmail.com>
2024-04-18 00:46:36 +0200

    user entity identity

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,4 +1,6 @@
 using Infrastructure.Contexts;
+using Infrastructure.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,7 +8,7 @@ builder.Services.AddControllersWithViews();
 
 
 builder.Services.AddDbContext<ApplicationContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("WebApp_Database")));
-
+builder.Services.AddIdentity<UserEntity, IdentityRole>().AddEntityFrameworkStores<ApplicationContext>();
 
 
 var app = builder.Build();

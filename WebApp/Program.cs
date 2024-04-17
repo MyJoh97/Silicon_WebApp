4e9fa0d9e21d51fe4d4dde2e653f619434e14534
My <my.j97@protonmail.com>
2024-04-18 00:56:17 +0200

    x.login

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -8,7 +8,20 @@ builder.Services.AddControllersWithViews();
 
 
 builder.Services.AddDbContext<ApplicationContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("WebApp_Database")));
-builder.Services.AddIdentity<UserEntity, IdentityRole>().AddEntityFrameworkStores<ApplicationContext>();
+builder.Services.AddIdentity<UserEntity, IdentityRole>(x =>
+{
+    x.SignIn.RequireConfirmedAccount = false;
+    x.User.RequireUniqueEmail = true;
+    x.Password.RequiredLength = 8;
+
+}).AddEntityFrameworkStores<ApplicationContext>();
+builder.Services.ConfigureApplicationCookie(x =>
+{
+    x.LoginPath = "/signin";
+});
+
+
+
 
 
 var app = builder.Build();

6ecac07a97c0ba56f68a267012524fb6d90d156e
My <my.j97@protonmail.com>
2024-04-18 21:45:53 +0200

    name identifier account controller

--- WebApp/Controllers/AccountController.cs
+++ WebApp/Controllers/AccountController.cs
@@ -3,6 +3,8 @@ using Infrastructure.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 using WebApp.ViewModels;
 
 namespace WebApp.Controllers;
@@ -34,6 +36,16 @@ public class AccountController(UserManager<UserEntity> userManager, ApplicationC
                 user.PhoneNumber = user.PhoneNumber;
                 user.UserName = user.Email;
                 user.Bio = user.Bio;
+
+                var result = await _userManager.UpdateAsync(user);
+                if (result.Succeeded)
+                {
+                    TempData["StatusMessage"] = "Updated basic information successfully.";
+                }
+                else
+                {
+                    TempData["StatusMessage"] = "Unable to save basic information.";
+                }
             }
         }
         else
@@ -50,14 +62,34 @@ public class AccountController(UserManager<UserEntity> userManager, ApplicationC
     {
         if (TryValidateModel(model.Address!))
         {
+            var nameIdentifier = User.FindFirst(ClaimTypes.NameIdentifier)!.Value;
+            var user = await _context.Users.Include(i => i.Address).FirstOrDefaultAsync(x => x.Id == nameIdentifier);
+            if (user != null)
+            {
+                user.FirstName = user.FirstName;
+                user.LastName = user.LastName;
+                user.Email = user.Email;
+                user.PhoneNumber = user.PhoneNumber;
+                user.UserName = user.Email;
+                user.Bio = user.Bio;
 
+                var result = await _userManager.UpdateAsync(user);
+                if (result.Succeeded)
+                {
+                    TempData["StatusMessage"] = "Updated address information successfully.";
+                }
+                else
+                {
+                    TempData["StatusMessage"] = "Unable to save address information.";
+                }
+            }
         }
         else
         {
             TempData["StatusMessage"] = "Unable to save address information.";
         }
 
-        
+
         return RedirectToAction("Details", "Account");
     }
 }

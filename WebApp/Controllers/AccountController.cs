744a492dd78867f436bde3260af1c6fcfb0f9f6c
My <my.j97@protonmail.com>
2024-04-18 20:24:08 +0200

    account controller

---
+++ WebApp/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+public class AccountController : Controller
+{
+    public IActionResult Details()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult UpdateBasicInfo()
+    {
+
+        TempData["StatusMessage"] = "Unable to save information.";
+        return RedirectToAction("Details", "Account");
+    }
+
+    [HttpPost]
+    public IActionResult UpdateAddressInfo()
+    {
+        TempData["StatusMessage"] = "Unable to save information.";
+        return RedirectToAction("Details", "Account");
+    }
+}

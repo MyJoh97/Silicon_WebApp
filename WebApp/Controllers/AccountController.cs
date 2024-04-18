81ef1a165c67f6425bceb35da2f3e79e9845b9eb
My <my.j97@protonmail.com>
2024-04-18 21:28:40 +0200

    account controller

--- WebApp/Controllers/AccountController.cs
+++ WebApp/Controllers/AccountController.cs
@@ -1,26 +1,63 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Infrastructure.Contexts;
+using Infrastructure.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.ViewModels;
 
 namespace WebApp.Controllers;
 
-public class AccountController : Controller
+[Authorize]
+
+public class AccountController(UserManager<UserEntity> userManager, ApplicationContext context) : Controller
 {
+    private readonly UserManager<UserEntity> _userManager = userManager;
+    private readonly ApplicationContext _context = context;
+
     public IActionResult Details()
     {
-        return View();
+        var viewModel = new AccountDetailsViewModel();
+        return View(viewModel);
     }
 
     [HttpPost]
-    public IActionResult UpdateBasicInfo()
+    public async Task<IActionResult> UpdateBasicInfo(AccountDetailsViewModel model)
     {
+        if(TryValidateModel(model.Basic!))
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user != null)
+            {
+                user.FirstName = user.FirstName;
+                user.LastName = user.LastName;
+                user.Email = user.Email;
+                user.PhoneNumber = user.PhoneNumber;
+                user.UserName = user.Email;
+                user.Bio = user.Bio;
+            }
+        }
+        else
+        {
+            TempData["StatusMessage"] = "Unable to save basic information.";
+        }
 
-        TempData["StatusMessage"] = "Unable to save information.";
+        
         return RedirectToAction("Details", "Account");
     }
 
     [HttpPost]
-    public IActionResult UpdateAddressInfo()
+    public async Task<IActionResult> UpdateAddressInfo(AccountDetailsViewModel model)
     {
-        TempData["StatusMessage"] = "Unable to save information.";
+        if (TryValidateModel(model.Address!))
+        {
+
+        }
+        else
+        {
+            TempData["StatusMessage"] = "Unable to save address information.";
+        }
+
+        
         return RedirectToAction("Details", "Account");
     }
 }

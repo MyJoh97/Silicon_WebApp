c742bc3288d55e6152f5c5b1d7499cd59c843908
My <my.j97@protonmail.com>
2024-04-18 05:06:06 +0200

    register user

--- WebApp/Controllers/AuthController.cs
+++ WebApp/Controllers/AuthController.cs
@@ -1,14 +1,17 @@
-﻿using Infrastructure.Entities;
+﻿using Infrastructure.Contexts;
+using Infrastructure.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebApp.ViewModels;
 
 namespace WebApp.Controllers;
 
-public class AuthController(UserManager<UserEntity> userManager, SignInManager<UserEntity> signInManager) : Controller
+public class AuthController(UserManager<UserEntity> userManager, SignInManager<UserEntity> signInManager, ApplicationContext context) : Controller
 {
     private readonly UserManager<UserEntity> _userManager = userManager;
     private readonly SignInManager<UserEntity> _signInManager = signInManager;
+    private readonly ApplicationContext _context = context;
 
     public IActionResult SignUp()
     {
@@ -18,7 +21,30 @@ public class AuthController(UserManager<UserEntity> userManager, SignInManager<U
     [HttpPost]
     public async Task<IActionResult> SignUp(SignUpViewModel model)
     {
-        return RedirectToAction("Home", "Default");
+        if (ModelState.IsValid)
+        {
+            if (!await _context.Users.AnyAsync(x => x.Email == model.Email))
+            {
+                var userEntity = new UserEntity
+                {
+                    Email = model.Email,
+                    UserName = model.Email,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName
+                };
+
+
+                if ((await _userManager.CreateAsync(userEntity, model.Password)).Succeeded)
+                {
+
+                }
+            }
+
+            ViewData["StatusMessage"] = "User with the same email already exists";
+        }
+
+
+        return View(model);
     }
 
 
@@ -33,7 +59,7 @@ public class AuthController(UserManager<UserEntity> userManager, SignInManager<U
     {
         if (ModelState.IsValid)
         {
-            if((await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.IsPersistent, false)).Succeeded)
+            if ((await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.IsPersistent, false)).Succeeded)
                 return LocalRedirect(returnUrl);
         }
 
--- WebApp/ViewModels/SignUpViewModel.cs
+++ WebApp/ViewModels/SignUpViewModel.cs
@@ -2,4 +2,10 @@
 
 public class SignUpViewModel
 {
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string ConfirmPassword { get; set; } = null!;
+    public bool TermsAndConditions { get; set; }
 }

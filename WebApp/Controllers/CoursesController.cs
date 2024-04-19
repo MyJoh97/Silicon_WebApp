309adfb8ed6d43a1ecea77060dbd67e7dcbc1445
My <my.j97@protonmail.com>
2024-04-19 16:22:35 +0200

    course list course controller

--- WebApp/Controllers/CoursesController.cs
+++ WebApp/Controllers/CoursesController.cs
@@ -1,16 +1,28 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebApp.ViewModels;
 
 namespace WebApp.Controllers;
 
 [Authorize]
 
-public class CoursesController : Controller
+public class CoursesController(HttpClient httpClient) : Controller
 {
-    private readonly HttpClient _httpClient;
+    private readonly HttpClient _httpClient = httpClient;
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
+
+
+
+        var response = await _httpClient.GetAsync("https://localhost:7116/api/courses");
+        if (response.IsSuccessStatusCode)
+        {
+            var courses = JsonConvert.DeserializeObject<IEnumerable<CourseViewModel>>(await response.Content.ReadAsStringAsync());
+        }
+
+        
         return View();
     }
 }
